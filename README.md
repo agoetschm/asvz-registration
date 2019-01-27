4c7b63d486fdeea48634648257b6cfdb2a9712c1
agoestchm <anselme.goetschmann@gmail.com>
2019-01-27 21:06:08 +0100

    Make work on raspi

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 1. `npm install`
 2. rename `creds-template.js` to `creds.js` and edit it with your NETHZ credentials
-3. run `node asvz-registration.js <date> <lesson>` to schedule the registration of the lesson with id `<lesson>` at `<date>`, for example `node asvz-registration.js 2018-12-03T23:35:00 24659`
+3. go on the ASVZ webpage to get the id of the lesson you want to register for (in the URL)
+4. run `node asvz-registration.js <date> <lesson>` to schedule the registration of the lesson with id `<lesson>` at `<date>`, for example `node asvz-registration.js 13411 2019-01-27T20:55:00`
+
+### Note
+I had to replace `page.click('<selector>')` with `await page.evaluate(() => { document.querySelector('<selector>').click() })`, I don't know why the first one wasn't working on Raspbian.
--- asvz-registration.js
+++ asvz-registration.js
@@ -1,40 +1,51 @@
 const puppeteer = require('puppeteer');
 const schedule = require('node-schedule');
 
-const date = new Date(process.argv[2]);
-const lesson_number = process.argv[3];
+const lesson_number = process.argv[2];
 
-const job = schedule.scheduleJob(date, function(fireDate){
+const task = function(fireDate){
   console.log('Registering at ' + new Date() + " ...");
   (async () => {
     const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser'});
     const page = await browser.newPage();
+    // page.setDefaultNavigationTimeout(60000);
 
     await page.goto('https://schalter.asvz.ch/tn/lessons/' + lesson_number);
-    await page.click('body > app-root > div > div:nth-child(2) > app-lesson-details > div > div > app-lessons-enrollment-button > button');
+    console.log("Reached ASVZ page")
+    await page.evaluate(() => { document.querySelector('body > app-root > div > div:nth-child(2) > app-lesson-details > div > div > app-lessons-enrollment-button > button').click(); });
 
     await page.waitForNavigation();
-    await page.click('#switch-aai > div.panel-body > div > form > div > p > button');
+    await page.evaluate(() => { document.querySelector('#switch-aai > div.panel-body > div > form > div > p > button').click(); });
 
-    await page.waitFor(500);
-    await page.click('#userIdPSelection_iddwrap');
-    await page.click('#userIdPSelection_iddlist > div:nth-child(5)');
+    await page.waitForNavigation();
+    await page.evaluate(() => { document.querySelector('#userIdPSelection_iddwrap').click(); });
+    await page.evaluate(() => { document.querySelector('#userIdPSelection_iddlist > div:nth-child(5)').click(); });
 
-    await page.waitFor(500);
+    await page.waitForNavigation();
+    console.log("Reached login page")
     const CREDS = require('./creds');
-    await page.click('#username');
+    await page.focus('#username');
     await page.keyboard.type(CREDS.username);
-    await page.click('#password');
+    await page.focus('#password');
     await page.keyboard.type(CREDS.password);
-    await page.click('#LoginButtonText');
+    await page.evaluate(() => { document.querySelector('#LoginButtonText').click(); });
 
     await page.waitForSelector('#btnRegister');
-    await page.click('#btnRegister');
+    console.log("Logged in")
+    await page.evaluate(() => { document.querySelector('#btnRegister').click(); });
 
     console.log("Registration done.")
 
     await browser.close();
   })();
-});
+};
 
-console.log('Automatic registration for lesson ' + lesson_number + ' scheduled at ' + job.nextInvocation())
+if (process.argv[3]) {
+    const date = new Date(process.argv[3]);
+    const job = schedule.scheduleJob(date, task);
+    console.log('Automatic registration for lesson ' + lesson_number + ' scheduled at ' + job.nextInvocation());
+}
+else {
+    task();
+}
